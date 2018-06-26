0df48a049afa8dc45502c8a12c9c3afcc45d38a3
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-26 18:15:06 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -53,7 +53,7 @@ def list_songs_by_artist
       input = gets.strip.to_i
 
       if (1..Song.all.length).include?(input)
-        song = Song.all[input.length - 1]
+        song = Song.all[1..input.length - 1]
         puts "Playing #{song.name} by #{song.artist.name}"
       end
     end
