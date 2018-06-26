4ba4013646473f719a29bc0050b8428a88ca00c4
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-26 17:50:02 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -24,6 +24,21 @@ def call
 end
 end
 
+def list_songs_by_artist
+  puts "Please enter the name of an artist:"
+    msg = gets.strip
+
+    if msg = Artist.find_by_name(msg)
+      new_list = artist.songs.sort {|a,z| a <=> z}
+      new_list.each.with_index(1) do |song,i|
+        puts "#{i}. #{song.name} - #{song.genre.name}"
+      end
+    end
+  end
+
+
+
+
 def list_songs
   new_list = Song.all.sort{|a,z| a.name <=> z.name}
   new_list.each.with_index(1) do |song,i|
@@ -44,5 +59,7 @@ def list_genres
     puts "#{i}. #{genre.name}"
 end
 end
+
+
 
 end
